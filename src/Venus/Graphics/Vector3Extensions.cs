a058ef019fc8cd481f0dcd3dd2d2ccb034bd00ad
shnakamura <joaovtnakamura@gmail.com>
2026-08-10 00:18:11 -0300

    feat: graphics extensions

---
+++ src/Venus/Graphics/Vector3Extensions.cs
@@ -0,0 +1,11 @@
+﻿namespace Venus.Graphics;
+
+public static class Vector3Extensions
+{
+    extension(Vector3 vector)
+    {
+        public Vector2 ToVector2() => new Vector2(vector.X, vector.Y);
+        
+        public Vector4 ToVector4(float w = 0f) => new Vector4(vector.X, vector.Y, vector.Z, w);
+    }
+}
