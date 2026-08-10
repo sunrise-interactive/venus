06d5a5c9cfd438cd1c941f909392fcc1e0cce632
shnakamura <joaovtnakamura@gmail.com>
2026-08-10 12:58:17 -0300

    feat: adds sprite element

--- src/Venus/Graphics/Sprite.cs
+++ src/Venus/Graphics/Sprite.cs
@@ -1,6 +1,18 @@
 ﻿namespace Venus.Graphics;
 
-public struct Sprite
+public readonly record struct Sprite()
 {
-    
+    public readonly required Vector2 Position { get; init; }
+
+    public readonly Vector2 Scale { get; init; } = Vector2.One;
+
+    public readonly Vector2 Origin { get; init; } = new(0.5f);
+
+    public readonly Color Color { get; init; } = Color.White;
+
+    public readonly float Rotation { get; init; }
+
+    public readonly Rectangle? Frame { get; init; }
+
+    public readonly SpriteEffects Effects { get; init; } = SpriteEffects.None;
 }
