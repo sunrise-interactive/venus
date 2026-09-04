368aa23358abb015a48b1b344f64c8aa85b77a28
shnakamura <joaovtnakamura@gmail.com>
2026-09-04 00:45:39 -0300

    wip: rendering

---
+++ src/Venus/Graphics/_Rendering/RenderLayer.cs
@@ -0,0 +1,25 @@
+﻿namespace Venus.Graphics;
+
+public readonly record struct RenderLayer
+{
+    /// <summary>
+    ///     Gets the name of the render layer.
+    /// </summary>
+    public string Name { get; }
+
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="RenderLayer"/> struct with the specified name.
+    /// </summary>
+    /// <param name="name">
+    ///     The name of the render layer.
+    /// </param>
+    /// <exception cref="ArgumentException">
+    ///     <paramref name="name"/> is <see langword="null"/> or empty.
+    /// </exception>
+    internal RenderLayer(string name)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(name);
+
+        Name = name;
+    }
+}
---
+++ src/Venus/Graphics/_Rendering/_Commands/Sprite.cs
@@ -0,0 +1,47 @@
+﻿namespace Venus.Graphics;
+
+public readonly struct Sprite()
+{
+    /// <summary>
+    ///     Gets the position of the sprite, in screen coordinates.
+    /// </summary>
+    public readonly required Vector2 Position { get; init; }
+    
+    /// <summary>
+    ///     Gets the origin of the sprite.
+    /// </summary>
+    public readonly Vector2 Origin { get; init; }
+
+    /// <summary>
+    ///     Gets the scale of the sprite.
+    /// </summary>
+    public readonly Vector2 Scale { get; init; } = new(1f);
+
+    /// <summary>
+    ///     Gets the rotation of the sprite, in radians.
+    /// </summary>
+    public readonly float Rotation { get; init; }
+
+    /// <summary>
+    ///     Gets the opacity of the sprite.
+    /// </summary>
+    /// <value>
+    ///     A value in the range of <c>[0f - 1f]</c>, where <c>0f</c> represents fully transparent and <c>1f</c> represents fully opaque.
+    /// </value>
+    public readonly float Opacity { get; init; } = 1f;
+
+    /// <summary>
+    ///     Gets the color of the sprite.
+    /// </summary>
+    public readonly Color Color { get; init; } = Color.White;
+    
+    /// <summary>
+    ///     Gets the source rectangle of the sprite.
+    /// </summary>
+    public readonly Rectangle? Frame { get; init; }
+
+    /// <summary>
+    ///     Gets the effects of the sprite.
+    /// </summary>
+    public readonly SpriteEffects Effects { get; init; } = SpriteEffects.None;
+}
