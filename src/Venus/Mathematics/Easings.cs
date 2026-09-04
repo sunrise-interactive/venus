5e3e0ac706954680d8266f17734504a7f83a20c3
shnakamura <joaovtnakamura@gmail.com>
2026-09-04 19:11:17 -0300

    feat: easings

---
+++ src/Venus/Mathematics/Easings.cs
@@ -0,0 +1,60 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Venus.Mathematics;
+
+public static class Easings
+{
+    public static class Quadratic
+    {
+        /// <summary>
+        ///     Applies a quadratic easing function to the input value.
+        /// </summary>
+        /// <param name="t">
+        ///     The normalized input value to ease.
+        /// </param>
+        /// <returns>
+        ///     The eased value.
+        /// </returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float In(float t) => t * t;
+
+        /// <summary>
+        ///     Applies a quadratic easing function to the input value in reverse.
+        /// </summary>
+        /// <param name="t">
+        ///     The normalized input value to ease.
+        /// </param>
+        /// <returns>
+        ///     The eased value.
+        /// </returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float Out(float t) => 1f - In(1f - t);
+    }
+
+    public static class Cubic
+    {
+        /// <summary>
+        ///     Applies a cubic easing function to the input value.
+        /// </summary>
+        /// <param name="t">
+        ///     The normalized input value to ease.
+        /// </param>
+        /// <returns>
+        ///     The eased value.
+        /// </returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float In(float t) => t * t * t;
+
+        /// <summary>
+        ///     Applies a cubic easing function to the input value in reverse.
+        /// </summary>
+        /// <param name="t">
+        ///     The normalized input value to ease.
+        /// </param>
+        /// <returns>
+        ///     The eased value.
+        /// </returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float Out(float t) => 1f - In(1f - t);
+    }
+}
